a8286bd49dd718786761494a0edee04536f40008
Daniel Clough <115506656+daniellee862@users.noreply.github.com>
2022-12-21 17:26:15 +0000

    refactor till-addition

--- till-addition/__tests__/till-addition.test.js
+++ till-addition/__tests__/till-addition.test.js
@@ -2,23 +2,6 @@ const tillAddition = require("../till-addition");
 
 //ARRANGE. ACT. ASSERT.
 
-//toBe - same object/refernce
-//toEqual - to be equal to, but not the same instance.
-
-//test for return of new value
-// test('returns a new array', () => {
-//   const input = ['a', 'b', 'c'];
-//   const returnValue = createNewPeople(input);
-//   expect(input).not.toBe(returnValue);
-// });
-
-//test for non mutation of input
-// test('does not mutate the input array', () => {
-//   const input = ['a', 'b', 'c'];
-//   createNewPeople(input);
-//   expect(input).toEqual(['a', 'b', 'c']);
-// });
-
 //test list
 // empty input object should return "£0.00"
 // test return value is a string
@@ -26,21 +9,6 @@ const tillAddition = require("../till-addition");
 // return addition of multiple coins and multiple denominations
 //test return value is different from the input
 // test for mutated input
-
-/* const till = {
-    "1p": 1,
-    "2p": 1,
-    "5p": 1,
-    "10p": 1,
-    "20p": 1,
-    "50p": 1,
-    "£1": 1,
-    "£2": 1,
-    "£5": 1,
-    "£10": 1,
-    "£20": 1,
-    "£50": 1
-  }; */
 
 describe("till addition tests", () => {
   test(' empty input object should return "£0.00"', () => {
--- till-addition/till-addition.js
+++ till-addition/till-addition.js
@@ -1,61 +1,30 @@
 // Please do not change the name of this function
 
-/* # NOTE TO SELF; DANIEL.
-make use of look up tables for katas requiring rulesets of more than two conditions. */
-
 function tillAddition(cash) {
-  const cashArr = Object.entries(cash);
-
-  let total = 0;
-
-  const handleSum = (amount) => {
-    total += amount;
+  const coinTable = {
+    "1p": 0.01,
+    "2p": 0.02,
+    "5p": 0.05,
+    "10p": 0.1,
+    "20p": 0.2,
+    "50p": 0.5,
+    "£1": 1,
+    "£2": 2,
+    "£5": 5,
+    "£10": 10,
+    "£20": 20,
+    "£50": 50,
   };
 
-  cashArr.forEach((value) => {
-    switch (value[0]) {
-      case "1p":
-        handleSum(0.01 * value[1]);
-        break;
-      case "2p":
-        handleSum(0.02 * value[1]);
-        break;
-      case "5p":
-        handleSum(0.05 * value[1]);
-        break;
-      case "10p":
-        handleSum(0.1 * value[1]);
-        break;
-      case "20p":
-        handleSum(0.2 * value[1]);
-        break;
-      case "50p":
-        handleSum(0.5 * value[1]);
-        break;
-      case "£1":
-        handleSum(1 * value[1]);
-        break;
-      case "£2":
-        handleSum(2 * value[1]);
-        break;
-      case "£5":
-        handleSum(5 * value[1]);
-        break;
-      case "£10":
-        handleSum(10 * value[1]);
-        break;
-      case "£20":
-        handleSum(20 * value[1]);
-        break;
-      case "£50":
-        handleSum(50 * value[1]);
-        break;
-      default:
-      // code block
-    }
+  let sumOfAllCoins = 0;
+
+  const typesOfCoinAndAmounts = Object.entries(cash);
+
+  typesOfCoinAndAmounts.forEach((coinType) => {
+    sumOfAllCoins += coinTable[coinType[0]] * coinType[1];
   });
 
-  return `£${total.toFixed(2)}`;
+  return `£${sumOfAllCoins.toFixed(2)}`;
 }
 
 module.exports = tillAddition;
